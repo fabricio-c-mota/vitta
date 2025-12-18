bb1fb23cd6a5ab4e0c11f4cd8233164ef7a420a0
Fabrício Mota <capir.2024116TADS0002@aluno.ifpi.edu.br>
2025-12-18 10:57:36 -0300

    feat: adiciona repositório de agendamentos e configura Firebase

--- src/di/container.ts
+++ src/di/container.ts
@@ -2,11 +2,35 @@ import AuthUseCases from "@/usecase/auth/authUseCases";
 import { IAuthUseCases } from "@/usecase/auth/iAuthUseCases";
 import FirebaseAuthService from "@/infra/firebase/service/firebaseAuthService";
 import FirebaseUserRepository from "@/infra/firebase/repository/firebaseUserRepository";
+import FirebaseAppointmentRepository from "@/infra/firebase/repository/firebaseAppointmentRepository";
+import { IAppointmentRepository } from "@/model/repositories/iAppointmentRepository";
+import { IUserRepository } from "@/model/repositories/iUserRepository";
+import GetAvailableTimeSlotsUseCase, { IGetAvailableTimeSlotsUseCase } from "@/usecase/appointment/getAvailableTimeSlotsUseCase";
+import RequestAppointmentUseCase, { IRequestAppointmentUseCase } from "@/usecase/appointment/requestAppointmentUseCase";
+import ListPatientAppointmentsUseCase, { IListPatientAppointmentsUseCase } from "@/usecase/appointment/listPatientAppointmentsUseCase";
+import GetAppointmentDetailsUseCase, { IGetAppointmentDetailsUseCase } from "@/usecase/appointment/getAppointmentDetailsUseCase";
+import GetNutritionistUseCase, { IGetNutritionistUseCase } from "@/usecase/user/getNutritionistUseCase";
 
 const authService = new FirebaseAuthService();
-
-const userRepository = new FirebaseUserRepository();
+const userRepository: IUserRepository = new FirebaseUserRepository();
+const appointmentRepository: IAppointmentRepository = new FirebaseAppointmentRepository();
 
 const authUseCases: IAuthUseCases = new AuthUseCases(authService, userRepository);
 
-export { authUseCases };
+const getAvailableTimeSlotsUseCase: IGetAvailableTimeSlotsUseCase = new GetAvailableTimeSlotsUseCase(appointmentRepository);
+const requestAppointmentUseCase: IRequestAppointmentUseCase = new RequestAppointmentUseCase(appointmentRepository);
+const listPatientAppointmentsUseCase: IListPatientAppointmentsUseCase = new ListPatientAppointmentsUseCase(appointmentRepository);
+const getAppointmentDetailsUseCase: IGetAppointmentDetailsUseCase = new GetAppointmentDetailsUseCase(appointmentRepository);
+const getNutritionistUseCase: IGetNutritionistUseCase = new GetNutritionistUseCase(userRepository);
+
+export {
+    authUseCases,
+    userRepository,
+    appointmentRepository,
+    getAvailableTimeSlotsUseCase,
+    requestAppointmentUseCase,
+    listPatientAppointmentsUseCase,
+    getAppointmentDetailsUseCase,
+    getNutritionistUseCase,
+};
+
